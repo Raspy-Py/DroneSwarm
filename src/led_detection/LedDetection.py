2a5dfe906681aa075a43b66f52971e71f5d0efa4
annastasyshyn <anna.stasyshynn@gmail.com>
2024-11-03 13:23:32 +0200

    three colours detection

--- src/led_detection/LedDetection.py
+++ src/led_detection/LedDetection.py
@@ -3,29 +3,33 @@ import torch
 from torchvision.transforms import ToTensor
 class LedDetector(torch.nn.Module):
     def forward(self, X: torch.Tensor) -> torch.Tensor:
+        X = X.squeeze(0)
         self.width = X.shape[2]
-        result = torch.stackself.detect_led(X)
-        return result
+        # result = torch.stackself.detect_led(X, 1, 0, 0)
+        red = torch.argmin(torch.sqrt((X[2] - 1) * (X[2] - 1)) + X[0] + X[1])
+        green = torch.argmin(torch.sqrt((X[1] - 1) * (X[1] - 1)) + X[0] + X[2])
+        blue = torch.argmin(torch.sqrt((X[0] - 1) * (X[0] - 1)) + X[1] + X[2])
 
-    def detect_led(self, X: torch.Tensor) -> torch.Tensor:
-        X = X.squeeze(0)  
-        min_point = torch.argmin(torch.sqrt((X[0] - 1) * (X[0] - 1)) + X[1] + X[2])
-        row = min_point // self.width
-        col = min_point - (row * self.width)
-        coordinates = torch.stack([row, col])
+        red_coordintates = self.get_coordinates(red)
+        green_coordintates = self.get_coordinates(green)
+        blue_coordintates = self.get_coordinates(blue)
+
+        return torch.stack([red_coordintates, green_coordintates, blue_coordintates])
+
+    
+    def get_coordinates(self, X: torch.Tensor) -> torch.Tensor:
+
+        y = X // self.width
+        x = X - (y * self.width)
+        coordinates = torch.stack([x, y])
         return coordinates
 
 if __name__=='__main__':
+
     ld = LedDetector()
-    img = cv2.imread("data/led/1.jpg")
-    img_tensor = ToTensor()(img).unsqueeze(0)
-    print(ld.forward(img_tensor))
-
-    rtsp_url = f'rtsp://192.168.55.1:554/live/main_stream'
-    cap = cv2.VideoCapture(rtsp_url)
-
+    cap = cv2.VideoCapture(0)
     if not cap.isOpened():
-        print("Cannot open RTSP stream")
+        print("Cannot open cam stream")
         exit()
 
     while True:
@@ -33,11 +37,14 @@ if __name__=='__main__':
         if not ret:
             print("Failed to grab frame")
             break
+        led_coords = ld.forward(ToTensor()(frame))
+        print(led_coords[0])
+        cv2.circle(frame, tuple(led_coords[0].numpy()), radius=4, color=(0, 0, 255), thickness=4)
+        cv2.circle(frame, tuple(led_coords[1].numpy()), radius=4, color=(0, 255, 0), thickness=5)
+        cv2.circle(frame, tuple(led_coords[2].numpy()), radius=4, color=(255, 0, 0), thickness=4)
+        cv2.imshow('Webcam Stream', frame)
 
-        # cv2.imshow('RTSP Stream', frame)
-        print(ld.forward(ToTensor()(frame)))
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-
-    cap.release()
+    cap.release()    
     cv2.destroyAllWindows()
