b9448669f67ba1aa686df84779ab143eb84f7284
Oleh Basystyi <basystyo@gmail.com>
2024-12-11 23:59:11 +0000

    Remove images

--- scripts/to_pt.py
+++
@@ -1,6 +0,0 @@
-import torch
-from LedDetection import LedDetector
-model = LedDetector()
-model.eval()
-trace_model = torch.jit.trace(model, torch.Tensor(1, 3, 192, 256))  # (1, 3, 192, 256)
-trace_model.save("model.pt")
